4c0fdda4080338388571c0d5207dc44646423979
Jacob Perron <jacob@openrobotics.org>
2020-07-22 10:45:24 -0700

    Support waiting indefinitely for commands to complete
    Also added a 'verbose' option to watch output while waiting.
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>

--- demos/mininet_ros/emulate_network.py
+++ demos/mininet_ros/emulate_network.py
@@ -9,10 +9,18 @@ from .host_options import HostOptions
 from .topo import ManyHostsOneSwitchTopo
 
 
+def _waiting(hosts):
+    for host in hosts:
+        if host.waiting:
+            return True
+    return False
+
+
 def emulate_ros_network(
     *,
     host_options: List[HostOptions],
-    duration=5.0,
+    duration=None,
+    verbose=False,
 ):
     """
     Create an emulated network for running ROS nodes.
@@ -21,6 +29,9 @@ def emulate_ros_network(
        The length of the list determines the number of hosts in the emulated
        network.
     :param duration: How long to let the commands run on each host.
+      If None, then run the commands until they complete.
+    :param verbose: Prints interleaved command output from all hosts to the
+      screen.
     """
     # Tell mininet to print useful information
     setLogLevel('info')
@@ -40,19 +51,33 @@ def emulate_ros_network(
         print(f"Running command on host '{host.name}': {' '.join(options.command)}")
         host.sendCmd(options.command)
 
-    # Let commands run for some duration
-    # TODO(jacobperron): or until they have terminated
-    start_time = time.time()
-    while (time.time() - start_time) < duration:
-        time.sleep(0.1)
+    if duration is not None:
+        start_time = time.time()
 
-    # Interrupt commands
-    for host in net.hosts:
-        host.sendInt()
+    hosts_output = [''] * len(net.hosts)
+    interrupted = False
+    # Wait until commands have complete
+    while _waiting(net.hosts):
+        # Collect output
+        for i, host in enumerate(net.hosts):
+            output = host.monitor(10)
+            hosts_output[i] += output
+            if verbose:
+                print(output)
 
-    # Wait for output
-    for host in net.hosts:
-        print(f"Host '{host.name}' output:")
-        host.waitOutput(verbose=True)
+        # We already interrupted the processes, just waiting for remaining output
+        if interrupted:
+            continue
+
+        # Check for timeout and interrupt commands
+        if duration is not None and (time.time() - start_time) >= duration:
+            print('Timed out, interrupting commands')
+            for host in net.hosts:
+                if host.waiting:
+                    host.sendInt()
+            interrupted = True
+
+    for host, output in zip(net.hosts, hosts_output):
+        print(f"Host '{host.name}' output:\n{output}")
 
     net.stop()
